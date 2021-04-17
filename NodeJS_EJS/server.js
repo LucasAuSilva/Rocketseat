349cea27cf0ea9ad660d1f5bfed5add2f42bbbd1
LucasAuSilva <silvaaugustolucas@gmail.com>
2021-04-17 20:43:57 -0300

    ForEach ejs

--- NodeJS_EJS/server.js
+++ NodeJS_EJS/server.js
@@ -3,7 +3,40 @@ const server = express()
 
 server.set("view engine", "ejs")
 
-server.get("/home", (req, res) => res.render("pages/index"))
+server.get("/home", (req, res) => { 
+    const items = [
+        { 
+            title: "D",
+            message: "esenvolver em EJS"
+        },
+        {
+            title: "E",
+            message: "JS usa JS para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "uito facil eu acho"
+        },
+        {
+            title: "A",
+            message: "mo a lontrinha"
+        },
+        {
+            title: "I",
+            message: "nstall ejs"
+        },
+        {
+            title: "S",
+            message: "A PORRA TA GRANDE"
+        }
+    ]
+
+    const subtitle = "Uma linguagem de modelagem para criação de paginas HTML utilizando JS"
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle,
+    })
+})
 
 server.get("/sobre", (req, res) => res.render("pages/about"))
 
