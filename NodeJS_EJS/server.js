d1d77c3c059eb71bb29ded3505b29d0e64d1bf6c
LucasAuSilva <silvaaugustolucas@gmail.com>
2021-04-17 20:02:35 -0300

    organize

--- NodeJS_EJS/server.js
+++ NodeJS_EJS/server.js
@@ -3,8 +3,8 @@ const server = express()
 
 server.set("view engine", "ejs")
 
-server.get("/home", (req, res) => res.render("index"))
+server.get("pages/home", (req, res) => res.render("index"))
 
-server.get("/sobre", (req, res) => res.render("about"))
+server.get("pages/sobre", (req, res) => res.render("about"))
 
 server.listen(3000, () => console.log("rodando"))
