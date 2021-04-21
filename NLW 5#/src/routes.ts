369b6e130b758406e24862374b2785381ca58a42
LucasAuSilva <silvaaugustolucas@gmail.com>
2021-04-21 18:29:13 -0300

    messages table finished

--- NLW 5#/src/routes.ts
+++ NLW 5#/src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { SettingsController } from "./controllers/SettingsController";
 import { UsersController } from "./controllers/UsersController";
+import { MessagesController } from "./controllers/MessagesController";
 
 const settingsController = new SettingsController();
 const usersController = new UsersController()
+const messagesController = new MessagesController()
 const routes = Router();
 
 /**
@@ -18,6 +20,9 @@ const routes = Router();
 
 routes.post("/settings", settingsController.create)
 routes.post("/users", usersController.create)
+routes.post("/messages", messagesController.create)
+
+routes.get("/messages/:id", messagesController.showByUser)
 
 
 export { routes };
---
+++ NLW 5#/src/services/MessagesService.ts
@@ -0,0 +1,39 @@
+import { getCustomRepository } from "typeorm"
+import { MessagesRepository } from "../repositories/MessageRepository"
+
+interface IMessageCreate {
+    admin_id?: string;
+    text: string;
+    user_id: string;
+}
+
+
+class MessagesService {
+    
+    async create({ admin_id, text, user_id}: IMessageCreate) {
+        const messagesRepository = getCustomRepository(MessagesRepository)
+
+        const message = messagesRepository.create({
+            admin_id,
+            text,
+            user_id
+        })
+
+        await messagesRepository.save(message)
+
+        return message
+    }
+
+    async listByUser(user_id: string) {
+        const messagesRepository = getCustomRepository(MessagesRepository)
+
+        const list = await messagesRepository.find({
+            where: { user_id },
+            relations: ["user"],
+        })
+
+        return list;
+    }
+}
+
+export { MessagesService }
